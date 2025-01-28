8ddee899a87ea070dcd7e9d4e874b2874a5fc99d
rio-tsukano <xtsukano.rio@contract.dentsusoken.com>
2025-01-28 15:49:01 +0900

    [refactor]Remove unnecessary libraries in GenerateTransactionIdJose.spec.ts

--- lib/adapters/out/cfg/GenerateTransactionIdJose.spec.ts
+++ lib/adapters/out/cfg/GenerateTransactionIdJose.spec.ts
@@ -1,4 +1,3 @@
-import 'reflect-metadata';
 import { describe, it, expect } from 'vitest';
 import { createGenerateTransactionIdJoseInvoker } from './GenerateTransactionIdJose';
 import { TransactionId } from '../../../domain';
